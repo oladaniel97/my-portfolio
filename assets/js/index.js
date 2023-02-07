e517b729d72a8bdf03613ace08667689b6d7735e
daniel amodu <danielolayinka97@gmail.com>
2023-02-07 20:42:39 +0100

    in progress

---
+++ assets/js/index.js
@@ -0,0 +1,9 @@
+function selectTab(tabIndex) {
+    //Hide All Tabs
+    document.getElementById("tab1Content").style.display = "none";
+    document.getElementById("tab2Content").style.display = "none";
+    document.getElementById("tab3Content").style.display = "none";
+    //Show the Selected Tab
+    document.getElementById("tab" + tabIndex + "Content").style.display =
+    "block";
+ }
