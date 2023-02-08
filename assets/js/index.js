7e0951f0468d0e88acf5e3e978724e11ff946551
daniel amodu <danielolayinka97@gmail.com>
2023-02-08 17:34:38 +0100

    .

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,3 +8,20 @@ function selectTab(tabIndex) {
     "block";
  }
 
+
+<div class="card">
+                    <img src="assets/images/netherland.bmp" alt="" class="img-fluid h-75">
+                    <p>A building designed for a client in Netherland</p>
+                </div>
+                <div class="card">
+                    <img src="assets/images/semi-detached2.jpg" alt="" class="img-fluid">
+                    <p>A semi-detached duplex in Lagos,Nigeria</p>
+                </div>
+                <div class="card">
+                    <img src="assets/images/3 bedroom.png" alt="" class="img-fluid">
+                    <p>3 bedroom Apartment for a client</p>
+                </div>
+                <div class="card">
+                    <img src="assets/images/semi-detached3.png" alt="" class="img-fluid">
+                    <p>Semi-detached duplex for a company</p>
+                </div>
