6461700f892cee4fa5f92d14285a358a4efba0f3
daniel amodu <danielolayinka97@gmail.com>
2023-04-13 01:41:58 +0100

    .

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,7 @@
 function selectTab(tabIndex) {
     //Hide All Tabs
     document.getElementById("tab1Content").style.display = "none";
-    document.getElementById("tab2Content").style.display = "none";
+    
     document.getElementById("tab3Content").style.display = "none";
     //Show the Selected Tab
     document.getElementById("tab" + tabIndex + "Content").style.display =
@@ -9,19 +9,3 @@ function selectTab(tabIndex) {
  }
 
 
-<div class="card">
-                    <img src="assets/images/netherland.bmp" alt="" class="img-fluid h-75">
-                    <p>A building designed for a client in Netherland</p>
-                </div>
-                <div class="card">
-                    <img src="assets/images/semi-detached2.jpg" alt="" class="img-fluid">
-                    <p>A semi-detached duplex in Lagos,Nigeria</p>
-                </div>
-                <div class="card">
-                    <img src="assets/images/3 bedroom.png" alt="" class="img-fluid">
-                    <p>3 bedroom Apartment for a client</p>
-                </div>
-                <div class="card">
-                    <img src="assets/images/semi-detached3.png" alt="" class="img-fluid">
-                    <p>Semi-detached duplex for a company</p>
-                </div>
